9caecc902026439318fef3d700fb3c432b5db9c0
Geovanna Santiago <geovanna_santiago@hotmail.com>
2020-10-10 15:43:43 -0400

    passando o css para o Search.vue

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -17,8 +17,8 @@
 <style>
 .buscador {
     display: flex;
-    margin-top: 70px;
-    justify-content: center;
+    margin-top: 30px;
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
 }
 
 #txt_busca{
@@ -33,6 +33,15 @@
     background-color: #007bff;
     border-color: #007bff;
     color: white;
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
     border-radius: .25rem;
+}
+
+.resultado {
+    display: inline-block;
+    justify-content: center;
+    margin-top: 30px;
+    font-weight: bolder;
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
 }
 </style>
