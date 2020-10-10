1934f25e8b68a2b41ab78b8ed7a9dcf5f2f3d120
keven Costa <kevensilvacosta@hotmail.com>
2020-10-10 15:44:40 -0400

    esse troço

--- src/Ipega/produtos.js
+++ src/Ipega/produtos.js
@@ -3,6 +3,6 @@ import { http } from './config'
 export default{
     
     listar:() => {
-        return http.get('')
+        return http.get()
     }
 }
--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,12 +1,20 @@
 <template>
     <header>
         <div class="buscador">
-            <input type="text" id="txt_busca" name="text" placeholder="Digite o código de barras aqui">
-            <button class="btn_busca">Buscar</button>
+            <input type="text" id="txt_busca" name="text" placeholder="Digite o código de barras aqui" v-model="codigo">
+            <button class="btn_busca" >Buscar</button>
         </div>
-        <tr>
-           <td></td>
-        </tr>
+        <div class="resultado">
+            <span class="nome_produto">Nome do produto: </span><br>
+            <span class="ncm_produto">NCM:</span><br>
+            <span class="desc_produto">Descrição:</span><br>
+            <span class="pais_produto">País de registro:</span><br>
+            <span class="fabricante_produto">Fabricante:</span><br>
+            <span class="distribuidor_produto">Distribuidor:</span><br>
+            <span class="categoria_produto">Categoria:</span><br>
+            <span class="marca_produto">Marca:</span><br>
+            <span class="preco_produto">Preço:</span>
+        </div>
     </header>
 </template>
 
@@ -17,8 +25,8 @@
 <style>
 .buscador {
     display: flex;
-    margin-top: 70px;
-    justify-content: center;
+    margin-top: 30px;
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
 }
 
 #txt_busca{
@@ -33,6 +41,15 @@
     background-color: #007bff;
     border-color: #007bff;
     color: white;
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
     border-radius: .25rem;
+}
+
+.resultado {
+    display: inline-block;
+    justify-content: center;
+    margin-top: 30px;
+    font-weight: bolder;
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
 }
 </style>
